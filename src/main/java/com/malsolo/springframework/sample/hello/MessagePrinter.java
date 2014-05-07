1d60235f7637e390c66aed3997dcc593fe36fe1b
Javier Beneito Barquero <jbbarquero@gmail.com>
2014-05-07 11:57:26 +0200

    Now using loggers (SLF4J+logback) instead of System.out

--- src/main/java/com/malsolo/springframework/sample/hello/MessagePrinter.java
+++ src/main/java/com/malsolo/springframework/sample/hello/MessagePrinter.java
@@ -1,16 +1,20 @@
 package com.malsolo.springframework.sample.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessagePrinter {
+	
+	static final Logger logger = LoggerFactory.getLogger(MessagePrinter.class);
 
 	@Autowired
     private MessageService service;
 
 	public void printMessage() {
-        System.out.println(this.service.getMessage());
+		logger.info(this.service.getMessage());
     }
 
 }
